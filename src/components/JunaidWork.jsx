c6380d1645ceb0245ae54b892fbe1af54bc2b4a5
junaidarshad1122 <jj8286014@gmail.com>
2026-03-11 12:12:46 -0700

    jk

---
+++ src/components/JunaidWork.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const JunaidWork = () => {
+  return (
+    <div>
+      <input type="text" placeholder='search' />
+      <button type='button'>clik</button>
+    </div>
+  )
+}
+
+export default JunaidWork
