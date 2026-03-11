7c6ab306309cb5d3200e706d94fc4ea99b599a61
ha2548493-glitch <ha2548493@gmail.com>
2026-03-11 11:24:27 +0500

    files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 import LearnRecharts from './components/LearnRecharts';
 import Zain from './components/Zain';
+import JunaidWork from './components/JunaidWork';
+import Abdullah from './components/Abdullah';
 
 function App() {
   return (
     <div className="App">
-  
+      <Abdullah/>
+  <JunaidWork/>
      {/* <LearnRecharts/> */}
      <Zain/>
+     
     </div>
   );
 }
