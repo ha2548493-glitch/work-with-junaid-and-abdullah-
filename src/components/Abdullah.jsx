c53fa2f439ae841a5dbaac38a2467f50670be57a
myfirstdemo <abdullahsaeeds836-star>
2026-03-11 11:18:33 +0500

    ChangeOne

---
+++ src/components/Abdullah.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Abdullah = () => {
+  return (
+    <div>
+        <h1>Name: Abdullah Saeed</h1>
+        <h1>RollNo:63206 </h1>
+        <h1>Password:1234</h1>
+    </div>
+  )
+}
+
+export default Abdullah
